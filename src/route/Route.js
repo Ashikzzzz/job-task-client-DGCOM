94adba99baa0fddfe52e05f2426ba765c40c0c70
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-07-17 15:54:59 +0600

    create home route

--- src/route/Route.js
+++ src/route/Route.js
@@ -10,6 +10,10 @@ export const router= createBrowserRouter([
         element: <Main></Main>,
         children: [
             {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
                 path: "/signup",
                 element: <SignUp></SignUp>
             },
