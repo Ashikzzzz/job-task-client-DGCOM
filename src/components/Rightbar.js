a7b4cab9e86785d61a8b6a8927601147df259604
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-07-17 17:39:01 +0600

    right part design done

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { FaAlignRight, FaArrowRight, FaBeer, FaCashRegister, FaLocationArrow, FaPowerOff, FaSearchLocation } from 'react-icons/fa';
+import { FaAlignRight, FaArrowRight, FaBeer, FaCashRegister, FaLocationArrow, FaPowerOff, FaRocketchat, FaSearchLocation } from 'react-icons/fa';
+import mega from "../assets/img/mega.png"
+
 
 const Rightbar = () => {
   return (
@@ -96,15 +98,43 @@ const Rightbar = () => {
             
         <div className='my-6'>
         <div className='flex items-center justify-evenly'>
-            <small className='text-gray-600 mr-80 '>Sold By</small>
-            
+            <small className='text-gray-600 mr-80 '>Sold By</small>    
         </div>
-       
-        
-        
-
+       <div className='flex items-center justify-evenly'>
+        <div className='mr-40'>
+            <h1 className='text-2xl'>DECATHLON</h1>
+        </div>
+        <div className='flex items-center'>
+                <FaRocketchat></FaRocketchat>
+                <h3 className='font-semibold text-cyan-600'>Chat</h3>
+        </div>
+       </div>
+       <div className='ml-8'>
+        <img className='w-1/3 h-16 ' src={mega}></img>
+       </div>
+        </div>
+        <div className=''>
+        <hr className="hr bg-red-500  w-full mx-auto mt-4" />
         </div>
 
+        <div className='flex items-center justify-evenly'>
+            <div>
+                <p><small className='text-gray-400'>Positive Seller Ratings</small></p>
+                <h2 className='text-3xl'>93%</h2>
+            </div>
+            <div>
+            <p><small className='text-gray-400'>Ship on time</small></p>
+            <h2 className='text-3xl'>100%</h2>
+            </div>
+            <div>
+            <p><small className='text-gray-400'>Chat response Rate</small></p>
+            <h2 className='text-3xl'>88%</h2>
+            </div>
+        </div>
+        <div className=''>
+        <hr className="hr bg-red-500  w-full mx-auto mt-4" />
+        </div>
+        <p className='text-cyan-600 font-bold'><small>View Store</small></p>
     </div>
   )
 }
