c309273b82f43f984bfb0a2b86a42165bcab11e1
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-07-17 16:23:11 +0600

    leftbar testing

--- src/components/Leftbar.js
+++ src/components/Leftbar.js
@@ -4,6 +4,7 @@ import shoe2 from "../assets/img/shoe2.jpg"
 const Leftbar = () => {
   return (
     <div className='bg-white w-full'>
+        
         <img className='w-full rounded' src={shoe2}></img>
     </div>
   )
