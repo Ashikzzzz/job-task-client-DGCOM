cea9a45d10f0deca4864249204a03b5cc7656455
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-07-18 11:01:06 +0600

    pie

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -17,7 +17,7 @@ const data=[
         <ResponsiveContainer width="100%" height="100%">
         <PieChart width={400} height={400}>
           <Pie data={data} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
-          {/* <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label /> */}
+          <Pie data={data} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
         </PieChart>
       </ResponsiveContainer>
         </div>
