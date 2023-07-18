9d9b94da80bac2b7f9fbaad3cf3c322d753643f5
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-07-18 18:21:55 +0600

    add picture

--- src/components/Leftbar.js
+++ src/components/Leftbar.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import shoe2 from "../assets/img/shoe2.jpg"
 
 const Leftbar = () => {
+  const [images,setImages]= useState([])
+
+useEffect(()=>{
+  fetch("http://localhost:5000/api/v1/image/getting-image-base64-formate")
+  .then(res => res.json())
+  .then(data =>{
+    console.log(data?.data)
+    setImages(data?.data)
+  })
+},[])
+
+
   return (
     <div className='bg-white w-full'>
         
-        <img className='w-full rounded' src={shoe2}></img>
+       <div>
+       <img className='w-full rounded' src={shoe2}></img>
+       </div>
+        {
+          images?.data?.map(image =>{
+            return <div>
+              <img src={image?.picture}></img>
+            </div>
+          })
+        }
     </div>
   )
 }
